0b51b6be090040df34c39a99d13e87724f71522f
Vivek Panchal <vicky.vivek14@gmail.com>
2020-01-15 15:49:44 -0500

    Updates

---
+++ precache-manifest.821d4a2f5e23fd22e995a153d18ae1ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "178a7b46f11a622ec57e4ede5cb0b8dc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "184f1f31935f74d7f77d",
+    "url": "/monsters-rolodex/static/css/main.53eb0bc6.chunk.css"
+  },
+  {
+    "revision": "1093a6240466003a5984",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.3a82cee6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "184f1f31935f74d7f77d",
+    "url": "/monsters-rolodex/static/js/main.d2e37b9d.chunk.js"
+  },
+  {
+    "revision": "fcc6c7087f08bfeac99a",
+    "url": "/monsters-rolodex/static/js/runtime-main.52149680.js"
+  }
+]);
